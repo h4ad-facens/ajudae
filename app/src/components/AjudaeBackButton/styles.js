2d118bc9d04c650ac1f94d0e641e85076c9269bc
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-10-04 14:17:58 -0300

    feat(ajudae-back-button): added back button component

---
+++ app/src/components/AjudaeBackButton/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const ButtonContainer = styled.TouchableOpacity`
+  width: 58px;
+  height: 58px;
+  background: #1890ff;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 24px;
+  margin-top: 24px;
+`;
