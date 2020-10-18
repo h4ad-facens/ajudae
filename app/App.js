17b5d1d20d9d7f7667ded1b2c6a83884d080b6c1
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-10-18 14:12:23 -0300

    feat(react-native-flipper): added support for flipper

--- app/App.js
+++ app/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 
 import UserContextProvider from './src/contexts/UserContext';
 import MainStack from './src/stacks/MainStack';
 
+const queryCache = new QueryCache();
+
+if (__DEV__) {
+  import('react-query-native-devtools').then(({ addPlugin }) => {
+    addPlugin(queryCache);
+  });
+}
+
 export default () => {
   return (
-    <UserContextProvider>
-      <NavigationContainer>
-        <MainStack /> 
-      </NavigationContainer>
-    </UserContextProvider>
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <UserContextProvider>
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
+      </UserContextProvider>
+    </ReactQueryCacheProvider>
   );
-}
+};
