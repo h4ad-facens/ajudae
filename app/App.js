9e22a6f46f93cd0c01070143a88fe3260493adfd
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-11-17 21:29:12 -0300

    refactor(app.js): added ignore to warning about timer

--- app/App.js
+++ app/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 
 import UserContextProvider from './src/contexts/UserContext';
@@ -8,6 +9,8 @@ import MainStack from './src/stacks/MainStack';
 const queryCache = new QueryCache();
 
 if (__DEV__) {
+  LogBox.ignoreLogs(['Setting a timer']);
+
   import('react-query-native-devtools').then(({ addPlugin }) => {
     addPlugin(queryCache);
   });
